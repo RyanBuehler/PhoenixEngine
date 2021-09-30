423d28aabbd48017fdf9f9054a577f1d0ea5d970
RyanBuehler <47442579+RyanBuehler@users.noreply.github.com>
2021-09-30 00:57:51 -0700

    ImGUI Basic Functionality

--- PhoenixEngine/src/Window.cpp
+++ PhoenixEngine/src/Window.cpp
@@ -4,6 +4,17 @@
 #include "Window.h"
 #include <chrono>
 
+#ifdef _IMGUI
+//TODO: ImGUI Test Stuff
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+#endif // _IMGUI
+
 Window::Window(const WindowProperties& properties) :
   m_pWindow(nullptr),
   m_WindowProperties(properties),
@@ -31,6 +42,50 @@ Window::Window(const WindowProperties& properties) :
     m_WindowProperties.Title.c_str(),
     NULL, NULL);
 
+  // TODO: What does this do?
+  glfwMakeContextCurrent(m_pWindow);
+
+  // TODO: VSync?
+  glfwSwapInterval(1);
+
+#ifdef _IMGUI
+  // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100
+  const char* glsl_version = "#version 100";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+  const char* glsl_version = "#version 150";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+  const char* glsl_version = "#version 130";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+  //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+  //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+
+  // Setup Platform/Renderer backends
+  ImGui_ImplGlfw_InitForOpenGL(m_pWindow, true);
+  ImGui_ImplOpenGL3_Init(glsl_version);
+#endif // _IMGUI
 
   // Verify the Window's creation
   if (!m_pWindow)
@@ -105,12 +160,31 @@ void Window::OnUpdate() noexcept
     m_SceneManager.GetCurrentSceneGameObjects(),
     m_SceneManager.GetCurrentSceneActiveCamera());
 
+#ifdef _IMGUI
+  // Start the Dear ImGui frame
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplGlfw_NewFrame();
+  ImGui::NewFrame();
+  bool open = true;
+  ImGui::ShowDemoWindow(&open);
+
+  ImGui::Render();
+
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+#endif // _IMGUI
+
   // Swap the back/front buffers
   glfwSwapBuffers(m_pWindow);
 }
 
 void Window::OnClose() noexcept
 {
+#ifdef _IMGUI
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+#endif // _IMGUI
+
   // Shut down the scenes
   m_SceneManager.Shutdown();
   // Close the window
